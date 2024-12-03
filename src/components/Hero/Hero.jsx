85da8bbd315bbaf7c3e524295b087ee412bb38c3
Harshit <sainiharshit32614@gmail.com>
2024-12-02 22:40:11 -0800

    Initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Harshit Saini</h1>
+        <p className={styles.description}>
+        I am a passionate and self-motivated Web Developer with a strong foundation in frontend technologies and a growing expertise in blockchain development.
+        I am proficient in tools and frameworks like React.js, HTML5, CSS3, and JavaScript.
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroimage.jpg")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
